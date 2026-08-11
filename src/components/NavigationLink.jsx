df4556b7e47f993d9b79de5e6adf847e2fbfca24
hanjo-syntax <hanjo.winter@tn.syntax-institut.de>
2026-08-11 11:09:16 +0200

    Use NavLink in Navigation Component

--- src/components/NavigationLink.jsx
+++ src/components/NavigationLink.jsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 export default function NavigationLink({ to, label }) {
     
     return (
-        <Link to={to} className="text-xl font-bold text-zinc-300 pb-2 hover:text-yellow-300 hover:border-b hover:border-yellow-300 hover:pb-2">
+        <NavLink
+            to={to}
+            className={({isActive}) =>
+                isActive ? "text-xl font-bold text-pink-500 pb-2 hover:text-yellow-300 hover:border-b hover:border-yellow-300 hover:pb-2"
+                           : "text-xl font-bold text-zinc-500 pb-2 hover:text-yellow-300 hover:border-b hover:border-yellow-300 hover:pb-2" 
+                }    
+            >
             { label }
-        </Link>
+        </NavLink>
     );
 }   
--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,9 +1,9 @@
 export const navigationLinks = [
   { to: '/', label: 'Startseite' },
   { to: '/menu', label: 'Speisekarte' },
-  { to: '/#feed', label: 'Der Feed' },
-  { to: '/#drops', label: 'Aktionen' },
-  { to: '/#visit', label: 'Besuch' },
+  { to: '/feed', label: 'Der Feed' },
+  { to: '/drops', label: 'Aktionen' },
+  { to: '/visit', label: 'Besuch' },
 ];
 
 export const navItems = navigationLinks;
@@ -11,5 +11,5 @@ export const navItems = navigationLinks;
 export const socialLinks = [
   { to: 'instagram', label: 'Instagram' },
   { to: 'tiktok', label: 'TikTok' },
-  { to: '#youtube', label: 'YouTube' },
+  { to: 'youtube', label: 'YouTube' },
 ];
